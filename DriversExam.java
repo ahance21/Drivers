a042c4c7d4019deff7dd0e32638d823cfdee9388
ahance21 <42975655+ahance21@users.noreply.github.com>
2019-03-18 12:41:11 -0400

    Add files via upload

---
+++ DriversExam.java
@@ -0,0 +1,32 @@
+
+public class DriversExam
+{
+
+	
+	private char[] correctAnswers;
+
+	
+	char[] studentAnswers = {'a', 'b', 'c'};
+	for(int i=0; i<= studentAnswers.length; i++)
+	System.out.print(studentAnswers[i]);
+	
+	public DriversExam(char[] c)
+		{   
+         char[] ans = c;
+         
+         
+         
+         
+		}
+
+
+	int totalCorrect()
+	{
+	  int correct = 0;
+	  for(int i=0; i <= correctAnswers.length; i++)
+	  while(correctAnswers[i].[i])
+	  correct++; 
+	}
+}
+
+
